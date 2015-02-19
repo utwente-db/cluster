db7e895d27ac5acca74f954f036a51003e86054e
Robin Aly <robin@aly.de>
2015-02-19 15:42:17 +0100

    Using the right zookeeper server. (Variable replacement required)

--- config/CLASSES/kafka_basic/kafka_basic.sh
+++ config/CLASSES/kafka_basic/kafka_basic.sh
@@ -6,10 +6,14 @@ mv kafka_2.8.0-0.8.1.1 /usr/lib/kafka
 mkdir -p /var/log/kafka
 chown -R hdfs /var/log/kafka
 
+# create dummy log file so that start script doesn't complain
+mkdir -p /usr/lib/kafka/log
+
 PLACE config/server.properties IN /usr/lib/kafka/config
 REPLACE the following variables 
   $COMPUTERID unique integer of computer (example mapping ctit035 -> 1035, farm13 -> 13)
   $CENTRAL zookeeper node (ctit048.ewi.utwente.nl or farmname.ewi.utwente.nl) 
 
+Also replace variables in 
 cat ./PATH_AND_ALIASES >> /etc/bash.bashrc
 cat ./PATH_AND_ALIASES >> /etc/profile

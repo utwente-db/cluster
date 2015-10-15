87623d4ac96143ebd303ee225aec62c63479ab33
Robin Aly <robin@aly.de>
2015-10-15 14:10:14 +0200

    Basic installation / configuraiton of Apache Spark 1.2.1.

--- config/CLASSES/spark_basic/install.sh
+++ config/CLASSES/spark_basic/install.sh
@@ -1,4 +1,7 @@
-apt-get install spark-core spark-python
+apt-get install spark-core spark-python scala
 
-# spark mainly works with scala
-apt-get install scala
+# copy configuraiton
+cp config/spark-defaults.conf /etc/spark/conf/
+
+# add global variable to every node
+export HADOOP_CONF_DIR=/etc/hadoop/conf
--- config/CLASSES/spark_master/install.sh
+++ config/CLASSES/spark_master/install.sh
@@ -1,8 +1,14 @@
 require spark_basic
 
+apt-get install spark-master spark-history-server
+
+# log history
 sudo -u hdfs hadoop fs -mkdir /user/spark 
 sudo -u hdfs hadoop fs -mkdir /user/spark/applicationHistory 
 sudo -u hdfs hadoop fs -chown -R spark:spark /user/spark
 sudo -u hdfs hadoop fs -chmod 1777 /user/spark/applicationHistory
 
-apt-get install spark-master spark-history-server
+# do once
+hdfs dfs -mkdir -p /user/spark/share/lib
+hdfs dfs -put /usr/lib/spark/lib/spark-assembly_*.jar /user/spark/share/lib/spark-assembly.jar
+

9526fb000e62c708d3eb15239f4503443eb92722
Robin Aly <robin@aly.de>
2016-04-11 14:29:55 +0200

    Added forced mode to shadow (ignores the time stamps of files)

--- scripts/shadow.py
+++ scripts/shadow.py
@@ -4,24 +4,31 @@ import sys
 import string
 
 def main():
-  src_dir, dst_dir = sys.argv[1:]
-  lsrc = len(src_dir)
+  args = sys.argv[1:]
   force = False
+  if args[0] == '-f':
+    force = True
+    args = args[1:]
+    
+  src_dir, dst_dir = args
+  lsrc = len(src_dir)
+
   for root, dirs, files in os.walk(src_dir):
     rel_root = root[lsrc+1:]
     dest_dir = os.path.join(dst_dir, rel_root)
     if not os.path.isdir(dest_dir):
       os.makedirs(dest_dir)
     
-    
     for name in files:
       src_fn = os.path.join(root, name)
       dst_fn = os.path.join(dest_dir, name)
-      os.path.join(root, name)
+      
+      print src_fn, dst_fn
       if os.path.isfile(dst_fn):
         if not force and os.path.getmtime(dst_fn) > os.path.getmtime(src_fn):
+          print "Older"
           continue
-      print src_fn, dst_fn
+      
       with open(src_fn) as f, open(dst_fn, 'w') as out:
         for line in f:
           t = string.Template(line)

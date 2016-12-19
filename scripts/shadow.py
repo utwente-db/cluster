8888716bcf2c15689242bb3705a0ef7f92199fc5
Robin Aly <robin@aly.de>
2016-12-19 15:21:38 +0100

    improved copy script

--- scripts/shadow.py
+++ scripts/shadow.py
@@ -2,6 +2,18 @@
 import os
 import sys
 import string
+import json
+
+def shadow_file(src_fn, dst_fn, force, env):
+  print src_fn, dst_fn
+  if os.path.isfile(dst_fn):
+    if not force and os.path.getmtime(dst_fn) > os.path.getmtime(src_fn):
+      print "Source file is older"
+      return
+  
+  with open(src_fn) as f, open(dst_fn, 'w') as out:
+    t = string.Template(f.read())
+    out.write(t.safe_substitute(env))
 
 def main():
   args = sys.argv[1:]
@@ -10,9 +22,19 @@ def main():
     force = True
     args = args[1:]
     
+  env = os.environ
+  if args[0] == '-e':
+    with open(args[1]) as f:
+      env = json.load(f)
+    args = args[2:]
+    
   src_dir, dst_dir = args
   lsrc = len(src_dir)
-
+  
+  if os.path.isfile(src_dir):
+    shadow_file(src_dir, dst_dir, force, env)
+    sys.exit(0)
+  
   for root, dirs, files in os.walk(src_dir):
     rel_root = root[lsrc+1:]
     dest_dir = os.path.join(dst_dir, rel_root)
@@ -22,17 +44,8 @@ def main():
     for name in files:
       src_fn = os.path.join(root, name)
       dst_fn = os.path.join(dest_dir, name)
-      
-      print src_fn, dst_fn
-      if os.path.isfile(dst_fn):
-        if not force and os.path.getmtime(dst_fn) > os.path.getmtime(src_fn):
-          print "Older"
-          continue
-      
-      with open(src_fn) as f, open(dst_fn, 'w') as out:
-        for line in f:
-          t = string.Template(line)
-          out.write(t.safe_substitute(os.environ))
+          
+      shadow_file(src_fn, dest_fn, force, env)
         
 if __name__ == '__main__':
   main()

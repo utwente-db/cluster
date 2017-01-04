96d617507a427ae577c0a0cf9a8151836914b4ae
Robin Aly <robin@aly.de>
2017-01-04 10:50:01 +0100

    Removing comments etc

--- scripts/install03HBase12.sh
+++ scripts/install03HBase12.sh
@@ -8,30 +8,23 @@ if [ ! -f "$FILE" ]; then
   export CENTRALNODE=farm02
   export DEBIAN_FRONTEND=noninteractive
   
-  # adjust file permissions
-  chown hbase /etc/hbase/conf/hbase.keytab 
-
   # temporarily deactivate starting of services
   # https://major.io/2014/06/26/install-debian-packages-without-starting-daemons/
-# cat > /usr/sbin/policy-rc.d << EOF
-# #!/bin/sh
-# echo "All runlevel operations denied by policy" >&2
-# exit 101
-# EOF
+cat > /usr/sbin/policy-rc.d << EOF
+#!/bin/sh
+echo "All runlevel operations denied by policy" >&2
+exit 101
+EOF
 
   # install/update hadoop, keeping already installed configuration files
   # -o Dpkg::Options::="--force-confold"
   apt-get -o Dpkg::Options::="--force-confold" install --yes --no-install-recommends zookeeper hbase
   
   # activate starting of services from debian packages again
-  # rm /usr/sbin/policy-rc.d
+  rm /usr/sbin/policy-rc.d
   
   # adjust file permissions
   chown hbase /etc/hbase/conf/hbase.keytab 
-  
-  # if [ -f "/etc/init.d/hbase-regionserver" ]; then
-  #   /etc/init.d/hbase-regionserver restart
-  # fi
   
   # mark job as done
   date > $FILE
